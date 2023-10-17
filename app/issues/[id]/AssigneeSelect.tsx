8bf3fca168f5bc1412f469e601827646c8199c71
davidho <dklho@hendrickscorp.com>
2023-10-17 14:53:02 +0800

    feat: Assign user at user detail page.

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,22 +1,69 @@
 'use client'
 
 import { Skeleton } from '@/app/components'
-import { User } from '@prisma/client'
+import { User, Issue } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 
 // Because this component required user interaction, must convert to client component.
-// Need to useEffects
+// Need to useEffects but ...
 
 // data:users is ANY, need to supply User array Type.
-function AssigneeSelect() {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
+// Also need to pass issue data as props
+function AssigneeSelect({ issue }: { issue: Issue }) {
+  const { data: users, error, isLoading } = useUsers()
+
+  if (isLoading) return <Skeleton />
+  if (error) return null
+
+  const assignIssue = (userId: string) => {
+    axios
+      .patch('/api/issues/' + issue.id, {
+        assignedToUserId: userId === 'Unassigned' ? null : userId,
+      })
+      .catch(() => {
+        toast.error('Changes could not be saved.')
+      })
+  }
+
+  // users map object is possibly undefined.
+  // Because initially when this component is rendered, Users is undefined until
+  // we fetch data from the backend.
+  // So we have to use optional chaining to fix the issue.
+
+  return (
+    // select user and patch api update
+    // Patch on select Change.
+    // if user is null then empty string, if empty string will auto select Unassigned.
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || 'Unassigned'}
+        onValueChange={assignIssue}
+      >
+        <Select.Trigger placeholder={'Assign...'} />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestion</Select.Label>
+            <Select.Item value='Unassigned'>Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
+  )
+}
+
+// custom user hook
+const useUsers = () =>
+  useQuery<User[]>({
     // unique identifying piece of data in the cache.
     queryKey: ['users'],
     // queryFn will return a promise that resolves to data.
@@ -26,31 +73,4 @@ function AssigneeSelect() {
     // retry up to 3 times to fetch data.
     retry: 3,
   })
-
-  if (isLoading) return <Skeleton />
-
-  if (error) return null
-
-  // users map object is possibly undefined.
-  // Because initially when this component is rendered, Users is undefined until
-  // we fetch data from the backend.
-  // So we have to use optional chaining to fix the issue.
-
-  return (
-    <Select.Root>
-      <Select.Trigger placeholder={'Assign...'} />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestion</Select.Label>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-          <Select.Item value={'1'}>David Ho</Select.Item>
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
-  )
-}
 export default AssigneeSelect
